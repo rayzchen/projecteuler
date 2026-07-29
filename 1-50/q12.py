cba2b043678c228a5af5d12c150f06b58c22975b
Ray Chen <tankimarshal2@gmail.com>
2026-07-30 00:27:01 +0100

    Simplify divisor counting using coprimality

--- 1-50/q12.py
+++ 1-50/q12.py
@@ -1,14 +1,29 @@
+"""
+Since n and n+1 are coprime, the number of divisors of
+n(n+1)/2 is either d(n/2) * d(n+1) or d(n) * d((n+1)/2)
+
+"""
+
 import math
 
 n = 0
+previous_divisors = 1
 while True:
     n += 1
-    T = n * (n + 1) // 2
     divisors = 0
-    for i in range(1, int(math.sqrt(T))):
-        if T % i == 0:
+    if n % 2 == 0:
+        to_check = n + 1
+    else:
+        to_check = (n + 1) // 2
+
+    for i in range(1, int(math.sqrt(to_check)) + 1):
+        if to_check % i == 0:
             divisors += 2
-    if divisors > 500:
+            if i ** 2 == to_check:
+                divisors -= 1
+
+    if previous_divisors * divisors > 500:
         break
-print(T) # 76576500
+    previous_divisors = divisors
+print(n * (n + 1) // 2) # 76576500
 
